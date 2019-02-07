56bb09dfd9b6d2ec012eb37344c11a6e1fb4e36a
Jasem Qabazard <jasemqabazard@yahoo.com>
2019-02-07 10:25:09 +0300

    downloads

--- ngsuccessarchitecture/src/app/components/homewellbeing/homewellbeing.component.ts
+++ ngsuccessarchitecture/src/app/components/homewellbeing/homewellbeing.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-homewellbeing',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homewellbeing.component.css']
 })
 export class HomewellbeingComponent implements OnInit {
+  ENGLISH: boolean;
+  ARABIC: boolean;
+  userlanguage: string = undefined;
+  userrole: string = undefined;
+  username: string = undefined;
+  realname: string = undefined;
+  USER: boolean;
+  ADMIN: boolean;
+  CUSTOMER: boolean;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.authService.loadUserCredentials();
+    this.subscription = this.authService.getUserrole()
+    .subscribe(
+      role => {
+        this.userrole = role;
+        if (role === undefined) {
+          this.USER = true;
+          this.CUSTOMER = false;
+          this.ADMIN = false;
+        } else if (role === 'CUSTOMER') {
+          this.USER = false;
+          this.CUSTOMER = true;
+          this.ADMIN = false;
+        } else if (role === 'ADMIN') {
+          this.ADMIN = true;
+          this.USER = false;
+          this.CUSTOMER = false;
+        }
+        // tslint:disable-next-line:max-line-length
+        console.log('role this.USER this.ADMIN this.CUSTOMER', role, this.USER, this.ADMIN, this.CUSTOMER);
+      });
   }
 
 }
