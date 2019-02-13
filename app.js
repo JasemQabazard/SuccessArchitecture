5cd0925ef848d952bf505130828ed907a87cc6c0
Jasem Qabazard <jasemqabazard@yahoo.com>
2019-02-13 12:38:21 +0300

    deploy

--- app.js
+++ app.js
@@ -42,14 +42,14 @@ const app = express();
 
 // Secure traffic only
 
-app.all('*', (req, res, next) => {
- if (req.secure) {
-   return next();
- }
- else {
-   res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
- }
-});
+// app.all('*', (req, res, next) => {
+//  if (req.secure) {
+//    return next();
+//  }
+//  else {
+//    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+//  }
+// });
 
 
 // view engine setup
