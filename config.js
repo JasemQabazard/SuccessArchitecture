320523637bee6ad7d12ca604414e075ffc1fa9b2
Jasem Qabazard <jasemqabazard@yahoo.com>
2019-02-13 12:30:49 +0300

    deploy

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+   'secretKey': '67890-54321-09876-12345',
+   'mongoUrl': 'mongodb://localhost:27017/SuccessArchitecture',
+   // amazon s3 security keys for s3-sa-bucket access by users s3-sa-store
+   accessKeyId: 'AKIAI3K7TC5RWNK4EGBQ',
+   secretAccessKey: '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'
+}
