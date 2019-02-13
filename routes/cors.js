619cd0dae92bfc7bf24f998657ccaa3a100483b1
Jasem Qabazard <jasemqabazard@yahoo.com>
2019-02-13 13:37:32 +0300

    deploy

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:4200'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:4200', 'https://enigmatic-journey-16576.herokuapp.com'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log('Origin', req.header('Origin'));
